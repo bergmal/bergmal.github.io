2cee05db02a1b305eec409e95c27fb55dc845e65
therandombadass <the.random.badass@gmail.com>
2016-11-23 20:19:10 +0100

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Bergmaal</title>
+		<link rel="stylesheet" type="text/css" href="../Resources/index-style.css">
+		<link rel="icon" href="../Resources/icon.ico.crdownload">
+	</head>
+	
+	
+	
+	<body>
+		<div id="pageContainer">
+			<header id="pageHeader">
+				<h1> Welcome to homepage </h1>
+				<p> The page made for no reason at all </p>
+			</header>
+			<div id="contentContainer" class="clearfix">
+				<nav id="pageNav">
+					<ul>
+						<li><a href="https://www.google.com/">Google.com</a></li>
+						<li><a href="login.php">Logg in</a></li>
+					</ul>
+				</nav>
+
+				<section id="pageSection">
+					<header class="sectionHeader">
+						<h2> Bergmaal homepage </h2>
+						<p> Home sweet home</p>
+					</header>
+					<article class="sectionArticle">
+						<p> this page exist to serve as a way for me to practice how to do webdesign and general programming and network systems </p>
+					</article>
+					<footer class="sectionFooter">
+						<p> this page is going to be changed frequently due to me learning new stuff, and also feeling like changing the webpage </p>
+					</footer>
+				</section>
+
+				<aside id="pageAside">
+					<p>random text in the side place</p>
+				</aside>
+			</div>
+			<br/>
+			<footer id="pageFooter">
+				<p> all rights are reserved Lars birger Bergmål </p>
+			</footer>
+		</div>
+	</body>
+</html>
