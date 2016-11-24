df795bba5ac062e87d25df7e0e2ffb7f187e0354
therandombadass <the.random.badass@gmail.com>
2016-11-24 23:20:02 +0100

    Add files via upload

---
+++ login (1).php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Sign-In</title>
+		<link rel="stylesheet" type="text/css" href="..\Resources\login-style.css">
+		<link rel="stylesheet" type="text/css" href="../Resources/icon.ico.crdownload">
+	</head>
+	<body id="body-color">
+		<div id="Sign-In">
+			<fieldset style="width:95%"><legend>LOG-IN HERE</legend>
+				<form method="POST" action="/Resources/conectivity.php">
+					<p> -Username: <input type="text" name="user" size="20" /> </p>
+					<p> -Password : <input type="password" name="pass" size="20" /> </p>
+					<br>
+					<p> <input id="button" type="submit" name="submit" value="Log-In"> </p>
+				</form> 
+			</fieldset>	 
+		</div> 
+	</body> 
+</html>
